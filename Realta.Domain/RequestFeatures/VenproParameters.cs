224431047cad7f7bb50116487d91d0817dcd46ae
Hafizmursiddd <hafizmursiddd@gmail.com>
2023-03-16 03:09:52 +0700

    add VenproParameters.cs dan repositoryvenproExtension

---
+++ Realta.Domain/RequestFeatures/VenproParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realta.Domain.RequestFeatures
+{
+    public class VenproParameters : RequestParameters
+    {
+        public string? Keyword { set; get; }
+        public string OrderBy { get; set; } = "vendorName";
+    }
+}
